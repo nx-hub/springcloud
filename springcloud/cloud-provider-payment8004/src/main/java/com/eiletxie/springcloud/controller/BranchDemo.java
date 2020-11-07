b986b805a3fb19a650017cbc20f5ae61672a693c
nx-hub <13469122801@163.com>
2020-11-07 13:50:44 +0800

    222

--- springcloud/cloud-provider-payment8004/src/main/java/com/eiletxie/springcloud/controller/BranchDemo.java
+++ springcloud/cloud-provider-payment8004/src/main/java/com/eiletxie/springcloud/controller/BranchDemo.java
@@ -3,8 +3,10 @@ package com.eiletxie.springcloud.controller;
 public class BranchDemo {
     public static void main(String[] args) {
         System.out.println("111");
-        System.out.println("222");
-        System.out.println("111");
-        System.out.println("222");
+        int a = 2;
+        int b=3;
+        int c=4;
+        System.out.println(a+b);
+
     }
 }
